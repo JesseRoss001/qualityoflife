27c1e6aea8c4a4c2974d8fba681b49a38c932ad4
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-13 12:12:59 +0000

    Add first chart no data bug

--- my_project/views.py
+++ my_project/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import CountryData
 from django.http import JsonResponse
 
+def your_view(request):
+    # Query to get the top 10 countries based on overall rank
+    top_countries = CountryData.objects.order_by('overall_rank')[:10].values('country_or_region', 'overall_rank')
+    
+    # Pass 'top_countries' to the template
+    return render(request, 'index.html', {'top_countries': top_countries})
 def index(request):
     return render(request, 'index.html')
 
